c6df9beb8bc87f7c8f5dfddddffdf2f85e12d27e
maya-fakih <alfakihmaya1@gmail.com>
2026-07-18 15:45:37 +0300

    plan: main format to be used for this node

--- src/perception_manager/src/perception_manager_node.cpp
+++ src/perception_manager/src/perception_manager_node.cpp
@@ -5,13 +5,27 @@ class PerceptionManager: public rclcpp::Node
     public:
         PerceptionManager(): Node("perception_manager")
         {
-
+            declare_parameter<float>("confidence_threshold", 0.5f);
+            // detections publisher
+            //set confidence service
+            //action server start_detection (handle goal/cancel/accepted)
+            
         }
 
     private:
+        //handle goal
+        //handle cancel
+        //handle accepted
+        //handle_set_confidence
+        //excute (main loop for action that we will bind the thread to)
+
+
+        //publisher for Detection msgs
+        //service server for SetConfidenceThreshold
+        // action server for StartDetection
+        // can add var for current goal track for bonus :D
 
 };
-a
 
 int main(int argc, char **argv)
 {
