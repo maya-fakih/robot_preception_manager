da91fba907b277b4766e97b7818c591ff910b3d4
maya-fakih <alfakihmaya1@gmail.com>
2026-07-18 16:00:34 +0300

    feat: set_confidence service configured with data checks in place

--- src/perception_manager/src/perception_manager_node.cpp
+++ src/perception_manager/src/perception_manager_node.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "costum_interfaces/msg/detection.hpp"
+#include "costum_interfaces/srv/set_confidence_threshold.hpp"
 
 class PerceptionManager: public rclcpp::Node
 {
@@ -12,8 +13,11 @@ class PerceptionManager: public rclcpp::Node
             // no need for wall timer here since it only fires when an action is running or is cancelled
             detections_pub_ = create_publisher<costum_interfaces::msg::Detection>("detections", 10);
             //set confidence service
+            service_ = create_service<costum_interfaces::srv::SetConfidenceThreshold>(
+                "set_confidence",
+                std::bind(&PerceptionManager::handle_set_confidence, this,
+                    std::placeholders::_1, std::placeholders::_2));
             //action server start_detection (handle goal/cancel/accepted)
-            
         }
 
     private:
@@ -27,6 +31,7 @@ class PerceptionManager: public rclcpp::Node
         //publisher for Detection msgs
         rclcpp::Publisher<costum_interfaces::msg::Detection>::SharedPtr detections_pub_;
         //service server for SetConfidenceThreshold
+        rclcpp::Service<costum_interfaces::srv::SetConfidenceThreshold>::SharedPtr service_;
         // action server for StartDetection
         // can add var for current goal track for bonus :D
 
