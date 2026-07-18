643029dcb9bb491589a22d58ea88dc6db5979dc3
maya-fakih <alfakihmaya1@gmail.com>
2026-07-18 16:12:57 +0300

    feat: setup action server bound to handle (goal, cancel, accept).

--- src/perception_manager/src/perception_manager_node.cpp
+++ src/perception_manager/src/perception_manager_node.cpp
@@ -1,10 +1,17 @@
 #include "rclcpp/rclcpp.hpp"
 #include "costum_interfaces/msg/detection.hpp"
 #include "costum_interfaces/srv/set_confidence_threshold.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "costum_interfaces/action/start_detection.hpp"
+using std::placeholders::_1;
+using std::placeholders::_2;
 
 class PerceptionManager: public rclcpp::Node
 {
     public:
+        using StartDetection = costum_interfaces::action::StartDetection;
+        using GoalHandleStartDetection = rclcpp_action::ServerGoalHandle<StartDetection>;
+ 
         PerceptionManager(): Node("perception_manager")
         {
             declare_parameter<float>("confidence_threshold", 0.5f);
@@ -15,9 +22,16 @@ class PerceptionManager: public rclcpp::Node
             //set confidence service
             service_ = create_service<costum_interfaces::srv::SetConfidenceThreshold>(
                 "set_confidence",
-                std::bind(&PerceptionManager::handle_set_confidence, this,
-                    std::placeholders::_1, std::placeholders::_2));
+                std::bind(&PerceptionManager::handle_set_confidence, this, _1, _2));
             //action server start_detection (handle goal/cancel/accepted)
+            action_server_ = rclcpp_action::create_server<StartDetection>(
+                this,
+                "start_detection",
+                std::bind(&PerceptionManager::handle_goal, this, _1, _2),
+                std::bind(&PerceptionManager::handle_cancel, this, _1),
+                std::bind(&PerceptionManager::handle_accepted, this, _1)
+            );
+
         }
 
     private:
@@ -25,6 +39,20 @@ class PerceptionManager: public rclcpp::Node
         //handle cancel
         //handle accepted
         //handle_set_confidence
+        void handle_set_confidence(
+            const std::shared_ptr<costum_interfaces::srv::SetConfidenceThreshold::Request> request,
+            std::shared_ptr<costum_interfaces::srv::SetConfidenceThreshold::Response>  response)
+        {
+            if (request->threshold <0.0f || request->threshold > 1.0f)
+            {
+                response->success = false;
+                response->message = "Threshold must be within [0.0, 1.0]";
+                return;
+            }
+            set_parameter(rclcpp::Parameter("confidence_threshold", request->threshold));
+            response->success = true;
+            response->message = "confidence_threshold updates successfully!";
+        }
         //excute (main loop for action that we will bind the thread to)
 
 
@@ -33,6 +61,7 @@ class PerceptionManager: public rclcpp::Node
         //service server for SetConfidenceThreshold
         rclcpp::Service<costum_interfaces::srv::SetConfidenceThreshold>::SharedPtr service_;
         // action server for StartDetection
+        rclcpp_action::Server<StartDetection>::SharedPtr action_server_;
         // can add var for current goal track for bonus :D
 
 };
