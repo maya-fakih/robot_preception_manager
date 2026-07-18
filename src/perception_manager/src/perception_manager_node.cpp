bd81279a1e843f6006edf164da52173bf6b1db2c
maya-fakih <alfakihmaya1@gmail.com>
2026-07-18 15:31:41 +0300

    base skeleton for Perception manager Node

--- src/perception_manager/src/perception_manager_node.cpp
+++ src/perception_manager/src/perception_manager_node.cpp
@@ -1,1 +1,23 @@
 #include "rclcpp/rclcpp.hpp"
+
+class PerceptionManager: public rclcpp::Node
+{
+    public:
+        PerceptionManager(): Node("perception_manager")
+        {
+            
+        }
+
+    private:
+
+};
+
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto perception_manager = std::make_shared<PerceptionManager>();
+    rclcpp::spin(perception_manager);
+    rclcpp::shutdown();
+    return (0);
+}
