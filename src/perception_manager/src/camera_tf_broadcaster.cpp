200c2c5650351068584b248e167b04fe8e91f664
maya-fakih <alfakihmaya1@gmail.com>
2026-07-18 17:07:49 +0300

    feat: created transform from fetched parameters and built the transform message.

--- src/perception_manager/src/camera_tf_broadcaster.cpp
+++ src/perception_manager/src/camera_tf_broadcaster.cpp
@@ -17,14 +17,30 @@ public:
         declare_parameter<double>("pitch", 0.0);
         declare_parameter<double>("yaw", 0.0);
         // create static broadcaster
+        broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
         // read params into local vars
+        double tx, ty, tz, roll, pitch, yaw;
+        get_parameter("tx", tx);
+        get_parameter("ty", ty);
+        get_parameter("tz", tz);
+        get_parameter("roll", roll);
+        get_parameter("pitch", pitch);
+        get_parameter("yaw", yaw);
         // build transform stamped msg
+        geometry_msgs::msg::TransformStamped transform;
+        transform.header.stamp = now();
+        transform.header.frame_id = "base_link";
+        transform.child_frame_id = "camera_optical_frame";
+        transform.transform.translation.x = tx;
+        transform.transform.translation.y = ty;
+        transform.transform.translation.z = tz;
         // convert roll pitch yaw to a quanternion
         // broadcast transform
     }
 
 private:
     // broadcaster_
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> broadcaster_;
 };
 
 int main(int argc, char ** argv)
